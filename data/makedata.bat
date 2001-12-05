b33d67200b8fa7a7a383fcb030b048d78f0ed3e6
nct <nct@localhost>
2001-12-05 11:07:28 +0000

    Added data building script for win32

---
+++ data/makedata.bat
@@ -0,0 +1,20 @@
+@echo Building data...
+
+type src\guiscr1a.png > gui01.data
+type src\guiscr1b1.png >> gui01.data
+type src\guiscr1b2.png >> gui01.data
+type src\guiscr1b3.png >> gui01.data
+type src\guiscr1b4.png >> gui01.data
+type src\guiscr1b5.png >> gui01.data
+type src\guiscr1b6.png >> gui01.data
+type src\guiscr2a.png > gui02.data
+type src\guiscr2b1.png >> gui02.data
+type src\guiscr2b2.png >> gui02.data
+type src\guiscr2b3.png >> gui02.data
+type src\guiscr3a.png > gui03.data
+type src\guiscr3b1.png >> gui03.data
+type src\guiscr3b2.png >> gui03.data
+type src\guiscr5a.png > gui05.data
+type src\guiscr5b1.png >> gui05.data
+type src\guiscr5b2.png >> gui05.data
+type src\guiscr5b3.png >> gui05.data
