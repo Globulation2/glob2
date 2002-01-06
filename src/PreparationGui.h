b7ad0f776fecf525d101fc7ed49c78f8d0de941b
nct <none@none>
2002-01-06 16:26:24 +0000

    Added GPL header

--- src/PreparationGui.h
+++ src/PreparationGui.h
@@ -1,7 +1,22 @@
 /*
- * Globulation 2 preparation gui
- * (c) 2001 Stephane Magnenat, Luc-Olivier de Charriere, Ysagoon
- */
+    Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charriere
+    for any question or comment contact us at nct@ysagoon.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
 
 #ifndef __PREPARATIONGUI_H
 #define __PREPARATIONGUI_H
@@ -18,7 +33,7 @@ public:
 	{
 		CAMPAIN = 1,
 		CUSTOM = 2,
-		MULTIPLAYERS = 3, 
+		MULTIPLAYERS = 3,
 		GAME_SETUP = 4,
 		QUIT = 5
 	};
