082cc9786a3d34b9322e127906d4d41dea12d72e
nuage <nuage@localhost>
2002-04-26 14:16:53 +0000

    Debugged the bug. Now the YOG Screen doesn't crash any more. The ugly static YOGScreen has been dispatched into two classes: -YOGScreen: only the GUI -YOGConnector: a clean way to connect to YOG Nothing more is static.

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -198,7 +198,7 @@ int Engine::initMutiplayerHost(void)
 
 	printf("Engine::the game is sharing ...\n");
 
-	MultiplayersHostScreen multiplayersHostScreen( &(multiplayersChooseMapScreen.sessionInfo) );//This line do trash YOGScreen::socketSet 
+	MultiplayersHostScreen multiplayersHostScreen( &(multiplayersChooseMapScreen.sessionInfo) );
 	int rc=multiplayersHostScreen.execute(globalContainer->gfx, 20);
 	if (rc==MultiplayersHostScreen::STARTED)
 	{
