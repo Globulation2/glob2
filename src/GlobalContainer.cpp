6f82d79f72c4c39c53faa94a97b5c1524399332e
nct <nct@localhost>
2003-01-21 18:40:16 +0000

    Added nice black

--- src/GlobalContainer.cpp
+++ src/GlobalContainer.cpp
@@ -1,20 +1,20 @@
 /*
-    Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charrière
+  Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charriï¿½e
     for any question or comment contact us at nct@ysagoon.com or nuage@ysagoon.com
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 #include "GlobalContainer.h"
@@ -46,6 +46,7 @@ GlobalContainer::GlobalContainer(void)
 	gfx=NULL;
 	terrain=NULL;
 	terrainShader=NULL;
+	terrainBlack=NULL;
 	ressources=NULL;
 	units=NULL;
 	buildings=NULL;
@@ -63,6 +64,8 @@ GlobalContainer::~GlobalContainer(void)
 		delete terrain;
 	if (terrainShader)
 		delete terrainShader;
+	if (terrainBlack)
+		delete terrainBlack;
 	if (ressources)
 		delete ressources;
 	if (units)
@@ -247,6 +250,9 @@ void GlobalContainer::load(void)
 
 		// load shader for unvisible terrain
 		terrainShader=gfx->loadSprite("data/gfx/shade");
+		
+		// black for unexplored terrain
+		terrainBlack=gfx->loadSprite("data/gfx/black");
 
 		updateLoadProgressBar(30);
 		// load ressources
