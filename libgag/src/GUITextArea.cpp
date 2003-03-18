b1b6fad8797d6fb7baff75fd26a14552a8a465c3
nct <none@none>
2003-03-18 21:04:07 +0000

    Fixed escape in texts

--- libgag/src/GUITextArea.cpp
+++ libgag/src/GUITextArea.cpp
@@ -1,20 +1,20 @@
 /*
-    Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charrière
+  Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charriï¿½e
     for any question or comment contact us at nct@ysagoon.com or nuage@ysagoon.com
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 #include <GUITextArea.h>
@@ -289,6 +289,10 @@ void TextArea::onSDLEvent(SDL_Event *event)
 			}
 			break;
 			
+			case SDLK_ESCAPE:
+			parent->onAction(this, TEXT_CANCELED, 0, 0);
+			break;
+			
 			case SDLK_RETURN:
 			if (!readOnly)
 			{
@@ -300,10 +304,13 @@ void TextArea::onSDLEvent(SDL_Event *event)
 			if (!readOnly)
 			{
 				Uint16 c=event->key.keysym.unicode;
-				char utf8text[4];
-				UCS16toUTF8(c, utf8text);
-				addText(utf8text);
-				parent->onAction(this, TEXT_MODIFIED, 0, 0);
+				if (c)
+				{
+					char utf8text[4];
+					UCS16toUTF8(c, utf8text);
+					addText(utf8text);
+					parent->onAction(this, TEXT_MODIFIED, 0, 0);
+				}
 			}
 			break;
 		}
@@ -424,7 +431,7 @@ void TextArea::internalSetText(const char *text)
 		temp[temppos]=0;
 		
 		// TODO : add getStringWidth with a number of char paramater to get ride of temp
-		// TODO : handle non­wrapping case
+		// TODO : handle nonwrapping case
 		while (pos<textBufferLength)
 		{
 			while ((font->getStringWidth(temp)<w-8)&&(pos<textBufferLength)&&(text[pos]!='\n'))
