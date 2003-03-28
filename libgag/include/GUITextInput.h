614a676970e63e42e50ac98c048f7eb82af0a38f
nct <none@none>
2003-03-28 15:12:36 +0000

    TextInput length can now be limited

--- libgag/include/GUITextInput.h
+++ libgag/include/GUITextInput.h
@@ -1,20 +1,20 @@
 /*
-    Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charrière
+  Copyright (C) 2001, 2002 Stephane Magnenat & Luc-Olivier de Charriï¿½e
     for any question or comment contact us at nct@ysagoon.com or nuage@ysagoon.com
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 #ifndef __GUITEXTINPUT_H
@@ -25,8 +25,8 @@
 class TextInput: public RectangularWidget
 {
 public:
-	TextInput(int x, int y, int w, int h, const Font *font, const char *text, bool activated);
-	virtual ~TextInput() { }
+	TextInput(int x, int y, int w, int h, const Font *font, const char *text, bool activated, unsigned textLength=512);
+	virtual ~TextInput() { delete[] text; }
 
 	virtual void onTimer(Uint32 tick);
 	virtual void onSDLEvent(SDL_Event *event);
@@ -36,17 +36,15 @@ public:
 
 protected:
 	const Font *font;
-	int cursPos;
-	int textDep;
+	unsigned cursPos;
+	unsigned textDep;
 	int cursorScreenPos;
 	virtual void repaint(void);
 	void recomputeTextInfos(void);
 	
 public:
-	enum {
-		MAX_TEXT_SIZE=512,
-	};
-	char text[MAX_TEXT_SIZE];
+	char *text;
+	unsigned textLength;
 	
 	bool activated;
 };
