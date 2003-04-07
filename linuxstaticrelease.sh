cd58991aa52256f0b4ee9732e86c760dfbc76c87
nct <nct@localhost>
2003-04-07 21:35:23 +0000

    Script for building static binaries under Linux

---
+++ linuxstaticrelease.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "Making glob2"
+make >& /dev/null
+cd src
+echo "Creating static binary"
+./staticlink.sh
+cd ..
+echo "Creating source distrib"
+make dist >& /dev/null
+echo "Decompressing dist archive in /tmp"
+gunzip -cd glob2-0.1.tar.gz | tar x -C/tmp
+echo "Patching archive with binary files"
+cp src/glob2 /tmp/glob2-0.1/src
+echo "Patching archive with map"
+cp ~/.glob2/*.map /tmp/glob2-0.1
+echo "Recompressing archive"
+cd /tmp
+tar cfz glob2-0.1-static.tar.gz glob2-0.1/
+scp glob2-0.1-static.tar.gz nct@lappc22.epfl.ch:~/public_html/
