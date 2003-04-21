3895f39645e668161611c9e4e5a14c561c94bb58
nuage <nuage@localhost>
2003-04-21 10:43:45 +0000

    Added AINumbi files to correct last cvs commit!.

---
+++ src/AINumbi.h
@@ -0,0 +1,73 @@
+/*
+  Copyright (C) 2001, 2002, 2003 Stephane Magnenat & Luc-Olivier de Charrière
+  for any question or comment contact us at nct@ysagoon.com or nuage@ysagoon.com
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+
+#ifndef __AI_NUMBI_H
+#define __AI_NUMBI_H
+
+#include "BuildingType.h"
+#include "AIImplementation.h"
+
+class Player;
+class Order;
+
+class AINumbi : public AIImplementation
+{
+public:
+	AINumbi(Player *player);
+	AINumbi(SDL_RWops *stream, Player *player);
+	void init(Player *player);
+	~AINumbi();
+
+	Player *player;
+	
+	bool load(SDL_RWops *stream);
+	void save(SDL_RWops *stream);
+	
+	Order *getOrder(void);
+	
+private:
+	int timer;
+	int phase;
+	int attackPhase;
+	int phaseTime;
+	int critticalWarriors;
+	int critticalTime;
+	int attackTimer;
+	int mainBuilding[BuildingType::NB_BUILDING];
+	int estimateFood(int x, int y);
+	int countUnits(void);
+	int countUnits(const int medicalState);
+	Order *swarmsForWorkers(const int minSwarmNumbers, const int nbWorkersFator, const int workers, const int explorers, const int warriors);
+	void nextMainBuilding(const int buildingType);
+	bool checkUIDRoomForBuilding(int px, int py, int width, int height);
+	int nbFreeAround(const int buildingType, int posX, int posY, int width, int height);
+	bool parseBuildingType(const int buildingType);
+	void squareCircleScann(int &dx, int &dy, int &sx, int &sy, int &x, int &y, int &mx, int &my);
+	bool findNewEmplacement(const int buildingType, int *posX, int *posY);
+	Order *mayAttack(int critticalMass, int critticalTimeout, Sint32 numberRequested);
+	Order *adjustBuildings(const int numbers, const int numbersInc, const int workers, const int buildingType);
+	Order *checkoutExpands(const int numbers, const int workers);
+	Order *mayUpgrade(const int ptrigger, const int ntrigger);
+};
+
+#endif
+
+ 
+
