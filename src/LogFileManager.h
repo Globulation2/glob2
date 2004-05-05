a5ccf2a5270dc95671d6456f094105dd5658c220
nuage <nuage@localhost>
2004-05-05 11:52:13 +0000

    Commented LogFileManager.

--- src/LogFileManager.h
+++ src/LogFileManager.h
@@ -26,6 +26,19 @@
 
 class FileManager;
 
+/*!
+The LogFileManager is an utility class. It's designed to have only one instance per programm.
+Simply call the class method "getFile()" like you would call the C function "fopen()".
+The returned "FILE*" is writeable, and stored in a place choosed by the "FileManager*" policy.
+
+The name of the file is the concathenation of the user name and the "const char *fileName" argument.
+This is usefull when you have to test multiple users on the same account while debugging multiplayers games.
+
+If you request the same file name more than once, the LogFileManager will give you a pointer "FILE*"
+to the same file, without opening nor closing any file.
+This way we can have all log data of all instances of the same class in the same file.
+*/
+
 class LogFileManager
 {
 public:
