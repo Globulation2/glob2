26346bd34e819858fef2343b3c5e78042434cce4
nct <none@none>
2004-05-31 14:29:27 +0000

    Fix some gfx configuration stuff

--- data/gfx/mkmini.sh
+++ data/gfx/mkmini.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 @ i=0
-while ($i < 37)
+while ($i < 46)
 	convert -resize 40x40 building${i}.png buildingmini${i}.png
 	convert -resize 40x40 building${i}r.png buildingmini${i}r.png
 	@ i = $i + 1
