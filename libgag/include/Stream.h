d0e676b409e419468632022cc03684e1d4ea921b
nct <nct@localhost>
2004-08-19 19:16:15 +0000

    Aded swimming pool level 0 half destroyed building

--- libgag/include/Stream.h
+++ libgag/include/Stream.h
@@ -23,14 +23,6 @@
 // For U/SintNN
 #include "Types.h"
 
-// For htons/htonl
-#ifdef WIN32
-	#include <windows.h>
-#else
-	#include <netinet/in.h>
-#endif
-#include <stdio.h>
-
 namespace GAGCore
 {
 	// Endian safe read and write
@@ -40,41 +32,22 @@ namespace GAGCore
 	{
 	public:
 		virtual ~OutputStream() { }
-	
-		virtual void write(const void *data, const size_t size, const char *name) = 0;
-	
-		virtual void flush(void) = 0;
-	
-		inline void writeEndianIndependant(const void *v, const size_t size, const char *name)
-		{
-			if (size==2)
-			{
-				*(Uint16 *)v = htons(*(Uint16 *)v);
-			}
-			else if (size==4)
-			{
-				*(Uint32 *)v = htonl(*(Uint32 *)v);
-			}
-			else if (size==8)
-			{
-				*(Uint32 *)v = htonl(*(Uint32 *)v);
-				*((Uint32 *)v+1) = htonl(*((Uint32 *)v+1));
-			}
-			write(v, size, name);
-		}
-	
-		inline void writeSint8(const Sint8 v, const char *name = NULL) { this->write(&v, 1, name); }
-		inline void writeUint8(const Uint8 v, const char *name = NULL) { this->write(&v, 1, name); }
-		inline void writeSint16(const Sint16 v, const char *name = NULL) { this->writeEndianIndependant(&v, 2, name); }
-		inline void writeUint16(const Uint16 v, const char *name = NULL) { this->writeEndianIndependant(&v, 2, name); }
-		inline void writeSint32(const Sint32 v, const char *name = NULL) { this->writeEndianIndependant(&v, 4, name); }
-		inline void writeUint32(const Uint32 v, const char *name = NULL) { this->writeEndianIndependant(&v, 4, name); }
-		inline void writeFloat(const float v, const char *name = NULL) { this->writeEndianIndependant(&v, 4, name); }
-		inline void writeDouble(const double v, const char *name = NULL) { this->writeEndianIndependant(&v, 8, name); }
 		
-		virtual void writeEnterSection(const char *name) { }
-		virtual void writeEnterSection(unsigned id) { }
-		virtual void writeLeaveSection(void) { }
+		virtual void write(const void *data, const size_t size, const char *name) = 0;
+		virtual void writeSint8(const Sint8 v, const char *name = NULL) = 0;
+		virtual void writeUint8(const Uint8 v, const char *name = NULL) = 0;
+		virtual void writeSint16(const Sint16 v, const char *name = NULL) = 0;
+		virtual void writeUint16(const Uint16 v, const char *name = NULL) = 0;
+		virtual void writeSint32(const Sint32 v, const char *name = NULL) = 0;
+		virtual void writeUint32(const Uint32 v, const char *name = NULL) = 0;
+		virtual void writeFloat(const float v, const char *name = NULL) = 0;
+		virtual void writeDouble(const double v, const char *name = NULL) = 0;
+		
+		virtual void flush(void) = 0;
+		
+		virtual void writeEnterSection(const char *name) = 0;
+		virtual void writeEnterSection(unsigned id) = 0;
+		virtual void writeLeaveSection(void) = 0;
 	};
 	
 	class InputStream
@@ -83,49 +56,18 @@ namespace GAGCore
 		virtual ~InputStream() { }
 	
 		virtual void read(void *data, size_t size, const char *name) = 0;
-	
-		inline void readEndianIndependant(void *v, size_t size, const char *name)
-		{
-			read(v, size, name);
-			if (size==2)
-			{
-				*(Uint16 *)v = ntohs(*(Uint16 *)v);
-			}
-			else if (size==4)
-			{
-				*(Uint32 *)v = ntohl(*(Uint32 *)v);
-			}
-			else if (size==8)
-			{
-				*(Uint32 *)v = ntohl(*(Uint32 *)v);
-				*((Uint32 *)v+1) = ntohl(*((Uint32 *)v+1));
-			}
-		}
-	
-		inline Sint8 readSint8(const char *name = NULL) { Sint8 i; this->read(&i, 1, name); return i; }
-		inline Uint8 readUint8(const char *name = NULL) { Uint8 i; this->read(&i, 1, name); return i; }
-		inline Sint16 readSint16(const char *name = NULL) { Sint16 i; this->readEndianIndependant(&i, 2, name); return i; }
-		inline Uint16 readUint16(const char *name = NULL) { Uint16 i; this->readEndianIndependant(&i, 2, name); return i; }
-		inline Sint32 readSint32(const char *name = NULL) { Sint32 i; this->readEndianIndependant(&i, 4, name); return i; }
-		inline Uint32 readUint32(const char *name = NULL) { Uint32 i; this->readEndianIndependant(&i, 4, name); return i; }
-		inline float readFloat(const char *name = NULL) { float f; this->readEndianIndependant(&f, 4, name); return f; }
-		inline double readDouble(const char *name = NULL) { double d; this->readEndianIndependant(&d, 8, name); return d; }
+		virtual Sint8 readSint8(const char *name = NULL) = 0;
+		virtual Uint8 readUint8(const char *name = NULL) = 0;
+		virtual Sint16 readSint16(const char *name = NULL) = 0;
+		virtual Uint16 readUint16(const char *name = NULL) = 0;
+		virtual Sint32 readSint32(const char *name = NULL) = 0;
+		virtual Uint32 readUint32(const char *name = NULL) = 0;
+		virtual float readFloat(const char *name = NULL) = 0;
+		virtual double readDouble(const char *name = NULL) = 0;
 		
-		virtual void readEnterSection(const char *name) { }
-		virtual void readEnterSection(unsigned id) { }
-		virtual void readLeaveSection(void) { }
-	};
-	
-	class BinaryFileStream : public OutputStream, public InputStream
-	{
-	private:
-		FILE *fp;
-	public:
-		BinaryFileStream(FILE *fp) { this->fp = fp; }
-		virtual ~BinaryFileStream() { fclose(fp); }
-		virtual void write(const void *data, const size_t size, const char *name) { fwrite(data, size, 1 ,fp); }
-		virtual void flush(void) { fflush(fp); }
-		virtual void read(void *data, size_t size, const char *name) { fread(data, size, 1, fp); }
+		virtual void readEnterSection(const char *name) = 0;
+		virtual void readEnterSection(unsigned id) = 0;
+		virtual void readLeaveSection(void) = 0;
 	};
 }
 
