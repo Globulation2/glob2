21ea3a55c674b800445f78de00574772b1d056a5
nct <none@none>
2004-09-19 16:54:30 +0000

    Language file have been splited

--- libgag/src/StringTable.cpp
+++ libgag/src/StringTable.cpp
@@ -20,6 +20,7 @@
 #include "StringTable.h"
 #include "Toolkit.h"
 #include "FileManager.h"
+#include <Stream.h>
 #include "assert.h"
 #include <iostream>
 
@@ -39,164 +40,154 @@ namespace GAGCore
 			delete strings[i];
 	}
 	
-	
-	/* file format is :
-	
-			number of language
-			name of string
-			string lang 1
-			.
-			.
-			.
-			string lang n
-	
-	
-		And fist space of each line is removed, to allow emptih strings.
-	
+	/*
+		Load, argument is a file listing the key file and translation files
+		
+		The key file contains all keys, each one on a different line
+		A translation file contains pair of key-value for the given translation
 	*/
-	
-	inline void terminateLine(char *s, int n)
-	{
-		for (int i=0; i<n; i++)
-		{
-			char c=s[i];
-			if (c=='\n')
-			{
-				s[i]=0;
-				return;
-			}
-			else if (c=='\r')
-			{
-				s[i]=0;
-				return;
-			}
-			else if (c==0)
-				return;
-		}
-	}
 	
 	bool StringTable::load(char *filename)
 	{
-		FILE *fp;
-		char temp[1024];
-		unsigned line=0;
+		std::string keyFile;
+		std::vector<std::string> translationFiles;
+		InputLineStream *inputLineStream;
 	
-		if ((fp = Toolkit::getFileManager()->openFP(filename, "r"))==NULL)
+		// Read index file
+		if ((inputLineStream = Toolkit::getFileManager()->openInputLineStream(filename)) == NULL)
 		{
 			return false;
 		}
 		else
 		{
-			actLang = 0;
-			
-			// get number of language
-			if (fgets(temp, 1024, fp)==NULL)
+			keyFile = inputLineStream->readLine();
+			while (!inputLineStream->isEndOfStream())
 			{
-				fclose(fp);
-				return true;
+				translationFiles.push_back(inputLineStream->readLine());
 			}
-			line++;
-			terminateLine(temp, 1024);
-			languageCount = atoi(temp);
-			actLang = std::min(actLang, languageCount);
-			defaultLang = std::min(defaultLang, languageCount);
-			
-			// read entries
-			while (!feof(fp))
+			delete inputLineStream;
+		}
+		
+		if (translationFiles.size() == 0)
+			return false;
+		
+		languageCount = translationFiles.size();
+		
+		// Temporary storage for keys and translations
+		std::vector<std::string> keys;
+		std::vector<std::map<std::string, std::string> > translations(translationFiles.size());
+		
+		// Load keys
+		if ((inputLineStream = Toolkit::getFileManager()->openInputLineStream(keyFile.c_str())) == NULL)
+		{
+			return false;
+		}
+		else
+		{
+			while (!inputLineStream->isEndOfStream())
 			{
-				OneStringToken *myString;
-				std::string key;
-	
-				// read from file and check format
-				if (fgets(temp, 1024, fp)==NULL)
-					break;
-				line++;
-				terminateLine(temp, 1024);
-				if (temp[0] != '[')
-					std::cerr << "StringTable::load: warning, lookup entry at line " << line << " lacks [ at start" << std::endl;
-				key = temp;
-				myString = new OneStringToken();
-	
-				// read all strings
-				for (int n=0; n<languageCount; n++)
+				keys.push_back(inputLineStream->readLine());
+			}
+			delete inputLineStream;
+		}
+		
+		// Load translations
+		for (size_t i=0; i<translationFiles.size(); i++)
+		{
+			if ((inputLineStream = Toolkit::getFileManager()->openInputLineStream(translationFiles[i].c_str())) == NULL)
+			{
+				return false;
+			}
+			else
+			{
+				while (!inputLineStream->isEndOfStream())
 				{
-					if (fgets(temp, 1024, fp)==NULL)
-					{
-						delete myString;
-						goto doublebreak;
-					}
-					line++;
-					terminateLine(temp, 1024);
-					if (temp==NULL)
-						myString->data.push_back("");
-					else
-						myString->data.push_back(temp);
+					const std::string &key = inputLineStream->readLine();
+					const std::string &value = inputLineStream->readLine();
+					translations[i][key] = value;
 				}
-				stringAccess[key] = strings.size();
-				strings.push_back(myString);
+				delete inputLineStream;
 			}
-			doublebreak:
-			fclose(fp);
-	
-			for (std::map<std::string, size_t>::iterator it=stringAccess.begin(); it!=stringAccess.end(); ++it)
+		}
+		
+		// Create entries
+		for (size_t i=0; i<keys.size(); i++)
+		{
+			const std::string &key = keys[i];
+			OneStringToken *entry = new OneStringToken;
+			
+			for (size_t j=0; j<translations.size(); j++)
 			{
+				std::map<std::string, std::string>::const_iterator it = translations[j].find(key);
+				if (it != translations[j].end())
+					entry->data.push_back(it->second);
+				else
+					entry->data.push_back("");
+			}
+			
+			stringAccess[key] = strings.size();
+			strings.push_back(entry);
+		}
+		
+		// Check for consistency
+		for (std::map<std::string, size_t>::iterator it=stringAccess.begin(); it!=stringAccess.end(); ++it)
+		{
+			bool lcwp=false;
+			int baseCountS=0;
+			int baseCountD=0;
+			const std::string &s = it->first;
+			for (size_t j=0; j<s.length(); j++)
+			{
+				char c = s[j];
+				if (lcwp && c!=' ' && c!='%')
+				{
+					if (c=='s')
+						baseCountS++;
+					else if (c=='d')
+						baseCountD++;
+					else
+					{
+						std::cerr << "text=(" << s << "), Only %d and %s are supported in translations !" << std::endl;
+						assert(false);
+						return false;
+					}
+				}
+				lcwp=(c=='%');
+			}
+			for (size_t i=0; i<strings[it->second]->data.size(); i++)
+			{
+				const std::string &s = strings[it->second]->data[i];
 				bool lcwp=false;
-				int baseCountS=0;
-				int baseCountD=0;
-				const std::string &s = it->first;
+				int countS=0;
+				int countD=0;
 				for (size_t j=0; j<s.length(); j++)
 				{
-					char c = s[j];
+					char c=s[j];
 					if (lcwp && c!=' ' && c!='%')
 					{
 						if (c=='s')
-							baseCountS++;
+							countS++;
 						else if (c=='d')
-							baseCountD++;
+							countD++;
 						else
 						{
-							std::cerr << "text=(" << s << "), Only %d and %s are supported in translations !" << std::endl;
+							std::cerr << "translation=(" << s << "Only %s and %d are supported in translations !" << std::endl;
 							assert(false);
 							return false;
 						}
 					}
 					lcwp=(c=='%');
 				}
-				for (size_t i=0; i<strings[it->second]->data.size(); i++)
+				if (baseCountS!=countS ||baseCountD!=countD)
 				{
-					const std::string &s = strings[it->second]->data[i];
-					bool lcwp=false;
-					int countS=0;
-					int countD=0;
-					for (size_t j=0; j<s.length(); j++)
-					{
-						char c=s[j];
-						if (lcwp && c!=' ' && c!='%')
-						{
-							if (c=='s')
-								countS++;
-							else if (c=='d')
-								countD++;
-							else
-							{
-								std::cerr << "translation=(" << s << "Only %s and %d are supported in translations !" << std::endl;
-								assert(false);
-								return false;
-							}
-						}
-						lcwp=(c=='%');
-					}
-					if (baseCountS!=countS ||baseCountD!=countD)
-					{
-						std::cerr << "text=]" << baseCountS << ":" << baseCountD << "](" << it->first << "), translation=[" << countS << ":" << countD << "](" << s << "), doesn't match !" << std::endl;
-						assert(false);
-						return false;
-					}
+					std::cerr << "In " << translationFiles[i] << ", text = [" << baseCountS << ":" << baseCountD << "] (" << it->first << "), translation = [" << countS << ":" << countD << "] (" << s << "), doesn't match !" << std::endl;
+					assert(false);
+					return false;
 				}
 			}
-			
-			return true;
 		}
+		
+		return true;
 	}
 	
 	
@@ -204,9 +195,9 @@ namespace GAGCore
 	{
 		for (std::map<std::string, size_t>::iterator it=stringAccess.begin(); it!=stringAccess.end(); ++it)
 		{
-			std::cout << "name=" << it->first << "\n";
+			std::cout << "name = " << it->first << "\n";
 			for (size_t i=0; i<strings[it->second]->data.size(); i++)
-				std::cout<< "trad[" << i << "]=" << strings[it->second]->data[i];
+				std::cout<< "trad[" << i << "] = " << strings[it->second]->data[i] << "\n";
 			std::cout << std::endl;
 		}
 	}
