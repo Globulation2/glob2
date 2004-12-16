c103829a4c06848450340770774100f14f89c62d
nct <nct@localhost>
2004-12-16 22:44:05 +0000

    Fixed some memory trash

--- libgag/src/StringTable.cpp
+++ libgag/src/StringTable.cpp
@@ -17,9 +17,9 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#include "StringTable.h"
-#include "Toolkit.h"
-#include "FileManager.h"
+#include <StringTable.h>
+#include <Toolkit.h>
+#include <FileManager.h>
 #include <Stream.h>
 #include "assert.h"
 #include <iostream>
