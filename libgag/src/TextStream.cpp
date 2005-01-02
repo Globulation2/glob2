860f164d4c84fcbdddbde512a7e7262ec3b330b0
nct <nct@localhost>
2005-01-02 16:08:17 +0000

    Added TextStream read

--- libgag/src/TextStream.cpp
+++ libgag/src/TextStream.cpp
@@ -18,7 +18,10 @@
 */
 
 #include <TextStream.h>
+#include <assert.h>
 #include <fstream>
+#include <iostream>
+#include <stack>
 
 namespace GAGCore
 {
@@ -30,6 +33,7 @@ namespace GAGCore
 	
 	void TextOutputStream::printString(const char *string)
 	{
+		assert(string);
 		backend->write(string, strlen(string));
 	}
 	
@@ -44,8 +48,8 @@ namespace GAGCore
 		const unsigned char *dataChars = static_cast<const unsigned char *>(data);
 		for (size_t i=0; i<size; i++)
 		{
-			char buffer[4];
-			snprintf(buffer, sizeof(buffer), "%02x ", dataChars[i]);
+			char buffer[3];
+			snprintf(buffer, sizeof(buffer), "%02x", dataChars[i]);
 			printString(buffer);
 		}
 		printString(";\n");
@@ -92,5 +96,399 @@ namespace GAGCore
 		level -= count;
 		printLevel();
 		printString("}\n");
+	}
+	
+	// Parser for TextInputStream
+	
+	struct Parser
+	{
+		// parser
+		struct Token
+		{
+			enum Type
+			{
+				END_OF_INPUT = 0,
+				OPEN_BRACKET,
+				CLOSE_BRACKET,
+				EQUAL,
+				SEMI_COLON,
+				VAL,
+				NONE
+			} type;
+			
+			std::string val;
+			
+			Token(Type type, const std::string &val = "")
+			{
+				this->type = type;
+				this->val = val;
+			}
+		};
+		
+		int next;
+		StreamBackend *stream;
+		Token token;
+		size_t line, column;
+		
+		Parser(StreamBackend *stream)
+		 : token(Token::NONE)
+		{
+			assert(stream);
+			
+			line = 1;
+			column = 0;
+			this->stream = stream;
+			
+			if (!stream->isEndOfStream())
+				nextChar();
+		}
+		
+		int nextChar(void)
+		{
+			next = stream->getc();
+			if 	(next == '\n')
+			{
+				column = 0;
+				line++;
+			}
+			else
+				column++;
+			return next;
+		}
+		
+		Token nextToken(void)
+		{
+			if (!stream->isEndOfStream())
+			{
+				switch (next)
+				{
+					case '/':
+					nextChar();
+					if (next == '/')
+					{
+						// single line comment
+						while ((next != '\n') && (next != '\r') && !stream->isEndOfStream())
+							nextChar();
+						while (((next == '\n') || (next == '\r')) && !stream->isEndOfStream())
+							nextChar();
+					}
+					else if (next == '*')
+					{
+						// multi line comment
+						nextChar();
+						while ((next != '*' || (nextChar(), next != '/')) && !stream->isEndOfStream())
+							nextChar();
+						nextChar();
+						
+						/* More human readable version.
+						nextChar();
+						while (!ifs.eof())
+						{
+							while ((next != '*') && !ifs.eof())
+								nextChar();
+							nextChar();
+							if (next == '/')
+							{
+								nextChar();
+								break;
+							}
+						}*/
+					}
+					else
+					{
+						std::cerr << "TextStream::lexer : error @ " << line << ':' << column << " : / or * expected" << std::endl;
+					}
+					token = nextToken();
+					break;
+					
+					case '{':
+					nextChar();
+					token = Token(Token::OPEN_BRACKET);
+					break;
+					
+					case '}':
+					nextChar();
+					token = Token(Token::CLOSE_BRACKET);
+					break;
+					
+					case '=':
+					nextChar();
+					token = Token(Token::EQUAL);
+					break;
+					
+					case ';':
+					nextChar();
+					token = Token(Token::SEMI_COLON);
+					break;
+					
+					case '"':
+					{
+						std::string tempValue;
+						while ((nextChar() != '"') && !stream->isEndOfStream())
+						{
+							tempValue += static_cast<std::string::value_type>(next);
+						}
+						nextChar();
+						token = Token(Token::VAL, tempValue);
+					}
+					break;
+					
+					case ' ':
+					case '\t':
+					case '\n':
+					case '\r':
+					while (std::isspace(next))
+						nextChar();
+					token = nextToken();
+					break;
+					
+					default:
+					{
+						if (isalnum(next) || (next == '.') || (next == '-'))
+						{
+							std::string tempValue;
+							do
+							{
+								tempValue += static_cast<std::string::value_type>(next);
+								nextChar();
+							}
+							while (isalnum(next) || (next == '.'));
+							token = Token(Token::VAL, tempValue);
+						}
+						else
+						{
+							std::cerr << "TextStream::lexer : error @ " << line << ':' << column << " : character or number or . expected" << std::endl;
+							token = nextToken();
+						}
+					}
+					break;
+				}
+			}
+			else
+			{
+				token = Token(Token::END_OF_INPUT);
+			}
+			return token;
+		}
+		
+		//! Macro that return false and an error message if parser encountered EOF
+		#define CHECK_NOT_EOF \
+		if (token.type == Token::END_OF_INPUT) \
+		{ \
+			 std::cerr << "TextStream::parser : error @ " << line << ':' << column << " : unexpected EOF" << std::endl; \
+			 return false; \
+		}
+		
+		//! Macro that return false and an error message if anything else then a value is encountered
+		#define CHECK_VAL \
+		if (token.type != Token::VAL) \
+		{ \
+			std::cerr << "TextStream::parser : error @ " << line << ':' << column << " : value expected" << std::endl; \
+			return false; \
+		}
+		
+		//! Macro that return false and an error message if anything else then a ; is encountered
+		#define CHECK_SEMICOLON \
+		if (token.type != Token::SEMI_COLON) \
+		{ \
+			std::cerr << "TextStream::parser : error @ " << line << ':' << column << " : ; (semicolon) expected" << std::endl; \
+			return false; \
+		}
+		
+		bool parse(std::map<std::string, std::string> *table)
+		{
+			if (stream->isEndOfStream())
+				return false;
+				
+			std::vector<std::string> levels;
+			std::string id;
+			std::string fullId;
+			std::stack<unsigned> autovectors; // stack of unsigned used for implicit vectors. If value is 0, the level is not considered using autovector
+			
+			assert(table);
+			
+			nextToken();
+			while (token.type != Token::END_OF_INPUT)
+			{
+				CHECK_NOT_EOF
+				if (token.type == Token::CLOSE_BRACKET)
+				{
+					if (levels.size())
+					{
+						// when using autovector, add a count member
+						if (autovectors.top() > 0)
+						{
+							std::ostringstream oss;
+							oss << autovectors.top();
+							(*table)[fullId + ".count"] = oss.str();
+						}
+						
+						autovectors.pop();
+						levels.pop_back();
+						fullId.clear();
+						
+						std::vector<std::string>::size_type size = levels.size();
+						std::vector<std::string>::size_type j = 0;
+						if (size != 0)
+						{
+							for (std::vector<std::string>::const_iterator i = levels.begin(); i != levels.end(); ++i)
+							{
+								fullId += *i;
+								if (++j < size)
+									fullId += ".";
+							}
+						}
+					}
+					else
+					{
+						std::cerr << "TextStream::parser : error @ " << line << ':' << column << " : bracket closed but none was opened" << std::endl;
+					}
+					nextToken();
+				}
+				else
+				{
+					if (token.type == Token::OPEN_BRACKET)
+					{
+						// autovector entry
+						std::ostringstream oss;
+						oss << autovectors.top()++;
+						id = oss.str();
+					}
+					else
+					{
+						// normal entry, have an id
+						CHECK_VAL
+						id = token.val;
+						
+						nextToken();
+						CHECK_NOT_EOF
+					}
+					
+					if (token.type == Token::EQUAL)
+					{
+						nextToken();
+						CHECK_NOT_EOF
+						CHECK_VAL
+						nextToken();
+						std::string val = token.val;
+						CHECK_SEMICOLON
+						std::string path = fullId;
+						if (fullId.length())
+							path += ".";
+						path += id;
+						(*table)[path] = val;
+						nextToken();
+					}
+					else if (token.type == Token::OPEN_BRACKET)
+					{
+						if (fullId.length())
+							fullId += ".";
+						fullId += id;
+						levels.push_back(id);
+						autovectors.push(0);
+						nextToken();
+						CHECK_NOT_EOF
+					}
+					else
+					{
+						std::cerr << "TextStream::parser : error @ " << line << ':' << column << " : = or { expected" << std::endl;
+						return false; 
+					}
+				}
+			}
+			
+			if (levels.size())
+			{
+				std::cerr << "TextStream::parser : error @ " << line << ':' << column << "End of file called while all } are not closed" << std::endl;
+				return false;
+			}
+			
+			return true;
+		}
+	};
+	
+	TextInputStream::TextInputStream(StreamBackend *backend)
+	{
+		Parser p(backend);
+		p.parse(&table);
+	}
+	
+	void TextInputStream::readEnterSection(const char *name)
+	{
+		levels.push_back(std::string(name));
+		key += ".";
+		key += name;
+	}
+	
+	void TextInputStream::readEnterSection(unsigned id)
+	{
+		std::ostringstream oss;
+		oss << id;
+		levels.push_back(oss.str());
+		key += ".";
+		key += oss.str();
+	}
+	
+	void TextInputStream::readLeaveSection(size_t count)
+	{
+		assert(count < levels.size());
+		
+		levels.resize(levels.size() - count);
+		key.clear();
+		size_t size = levels.size();
+		if (size != 0)
+		{
+			for (size_t i = 0; i < size; ++i)
+			{
+				key += levels[i];
+				if (i+1 < size)
+					key += ".";
+			}
+		}
+	}
+	
+	void TextInputStream::readFromTableToString(const char *name, std::string *result)
+	{
+		assert(result);
+		
+		std::string fullKey;
+		if (levels.size() > 0)
+		{
+			fullKey = key;
+			fullKey += ".";
+		}
+		fullKey += name;
+		
+		const std::map<std::string, std::string>::const_iterator it = table.find(fullKey);
+		if (it != table.end())
+		{
+			*result = it->second;
+		}
+		else
+		{
+			std::cerr << "TextInputStream::readFromTableToString : no entry named " << fullKey << std::endl;
+			*result = "";
+		}
+	}
+	
+	void TextInputStream::read(void *data, size_t size, const char *name)
+	{
+		std::string s;
+		readFromTableToString(name, &s);
+		size_t serializedLength = s.length() >> 1;
+		if (serializedLength != size)
+			std::cerr << "TextInputStream::read : requested length " << size << " differs from serialized size " << serializedLength << std::endl;
+		size_t toReadLength = std::min(size, serializedLength);
+		char *destBuffer = static_cast<char *>(data);
+		for (size_t i=0; i<toReadLength; i++)
+		{
+			char buffer[3];
+			unsigned val;
+			buffer[0] = s[i*2];
+			buffer[1] = s[(i*2)+1];
+			buffer[2] = 0;
+			sscanf(buffer, "%02x", &val);
+			destBuffer[i] = val;
+		}
 	}
 }
