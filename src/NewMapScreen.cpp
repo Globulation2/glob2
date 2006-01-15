722ac3b74b23a2ce8d67700193385d47d1cd5a68
nuage <nuage@localhost>
2006-01-15 23:59:59 +0000

    The assert header where missing.

--- src/NewMapScreen.cpp
+++ src/NewMapScreen.cpp
@@ -17,6 +17,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 #include "NewMapScreen.h"
+#include <assert.h>
 #include <Toolkit.h>
 #include <StringTable.h>
 #include <GraphicContext.h>
