6b5366e624b4c6fdcce9d55012317303ca5fb185
Bradley Arsenault <genixpro@gmail.com>
2007-04-26 20:43:18 -0400

    Frameworked NetConnection class

--- src/NetConnection.cpp
+++ src/NetConnection.cpp
@@ -17,3 +17,92 @@
 */
 
 #include "NetConnection.h"
+
+NetConnection::NetConnection(const std::string& address, Uint16 port)
+{
+	connected = false;
+	openConnection(address, port);
+}
+
+
+
+NetConnection::NetConnection()
+{
+
+}
+
+
+	
+void NetConnection::openConnection(const std::string& address, Uint16 port)
+{
+	if(!connected)
+	{
+		//Resolve the address
+		if(SDLNet_ResolveHost(&address, ipaddress.c_str(), port) == -1)
+		{
+			printf("NetConnection::openConnection: %s\n", SDLNet_GetError());
+			assert(false);
+		}
+		
+		//Open the connection
+		socket=SDLNet_TCP_Open(&address);
+		if(!socket)
+		{
+			printf("NetConnection::openConnection: %s\n", SDLNet_GetError());
+			assert(false);
+		}
+		else
+		{
+			connected=true;
+		}
+	}
+}
+
+
+
+void NetConnection::closeConnection()
+{
+	if(connected)
+		SDLNet_TCP_Close(socket);
+	connected=false;
+}
+
+
+
+bool NetConnection::isConnected()
+{
+	return connected;
+}
+
+
+
+Message NetConnection::getMessage()
+{
+
+}
+
+
+	
+void NetConnection::queueMessage(const Message& message)
+{
+
+}
+
+
+	
+void NetConnection::attemptConnection(TCPsocket& serverSocket)
+{
+	if(!connected)
+	{
+		socket=SDLNet_TCP_Accept(serverSocket);
+		if(!socket)
+		{
+		}
+		else
+		{
+			connected=true;
+		}
+	}
+}
+
+
