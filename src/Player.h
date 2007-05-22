15cdcd8db4aeb7447d01ff0ac89b1af759092126
Bradley Arsenault <genixpro@gmail.com>
2007-05-21 21:44:09 -0400

    Removed all of the lignering Net code in Player.h
    --HG--
    branch : core_rewrite_2007

--- src/Player.h
+++ src/Player.h
@@ -75,60 +75,11 @@ public:
 	char name[MAX_NAME_LENGTH];
 	Sint32 teamNumber;
 	Uint32 teamNumberMask;
-
-	IPaddress ip;
-	IPaddress yogip;
-	UDPsocket socket;
-	int channel;
-	bool ipFromNAT;
-	bool ipFirewallClean;
-	bool waitForNatResolution;
 	
 	bool quitting; // We have executed the quitting order of player, but we did not freed all his orders.
 	Uint32 quitUStep;
 	Uint32 lastUStepToExecute;
-	enum PlayerNetworkState
-	{
-		PNS_BAD=0,
-		
-		PNS_PLAYER_SILENT=1,
-		
-		// host choose a state for joiners between :
-		
-		PNS_PLAYER_SEND_PRESENCE_REQUEST=2,
-		PNS_PLAYER_SEND_SESSION_REQUEST=3,
-		PNS_PLAYER_SEND_CHECK_SUM=4,
-		
-		PNS_OK=5,
-		
-		PNS_SERVER_SEND_CROSS_CONNECTION_START=6,
-		PNS_PLAYER_CONFIRMED_CROSS_CONNECTION_START=7,
-		PNS_PLAYER_FINISHED_CROSS_CONNECTION=8,
-		
-		PNS_CROSS_CONNECTED=9,
-		
-		PNS_SERVER_SEND_START_GAME=10,
-		PNS_PLAYER_CONFIRMED_START_GAME=11,
-		PNS_PLAYER_PLAYS=12,
-		
-		// joiners choose a state for joiners between :
-		
-		PNS_SENDING_FIRST_PACKET=22,
-		PNS_HOST=23,
-		
-		// ai :
-		
-		PNS_AI=30
-	};
-	
-	PlayerNetworkState netState;
-	int netTimeout; // time before next action to repeat.
-	int netTimeoutSize;
-	int netTOTL; // Number of timeout allowed. TimeOut To Live
 
-private:
-	Uint8 data[32+MAX_NAME_LENGTH];
-	
 public:
 	
 	BasePlayer(void);
@@ -139,32 +90,12 @@ public:
 	void setTeamNumber(Sint32 teamNumber);
 	bool load(GAGCore::InputStream *stream, Sint32 versionMinor);
 	void save(GAGCore::OutputStream *stream);
-	
-	Uint8 getOrderType();
-	
-	Uint8 *getData(bool compressed);
-	bool setData(const Uint8 *data, int dataLength, bool compressed);
-	int getDataLength(bool compressed);
-	
+
 	Uint32 checkSum();
 	
 	void makeItAI(AI::ImplementitionID aiType);
-	
-	void setip(Uint32 host, Uint16 port);
-	void setip(IPaddress ip);
-	bool sameip(IPaddress ip);
-	bool localhostip();
-	bool bind(UDPsocket socket);
-	void unbind();
-	
-	bool send(Uint8 *data, int size);
-	bool send(Uint8 *data, int size, const Uint8 v);
-	bool send(const Uint8 v);
-	bool send(const Uint8 u, const Uint8 v);
-	bool send(const Uint8 u, const Uint8 v, const Uint32 checksum);
 
 public:
-	bool destroyNet;
 	bool disableRecursiveDestruction;
 	
 public:
