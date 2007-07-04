0463be49f5fe99c0aabb8594a64f74c4e1b3a9ea
Bradley Arsenault <genixpro@gmail.com>
2007-07-04 14:24:29 -0400

    Fixed shading for the fog of war

--- src/Game.cpp
+++ src/Game.cpp
@@ -2355,7 +2355,7 @@ inline void Game::drawMapFogOfWar(int left, int top, int right, int bot, int sw,
 					unsigned shadeValue = i0 + (i1<<1) + (i2<<2) + (i3<<3);
 					
 					if (shadeValue==15)
-						globalContainer->gfx->drawFilledRect((x<<5)+16, (y<<5)+16, 32, 32, 0, 0, 0, 40);
+						globalContainer->gfx->drawFilledRect((x<<5)+16, (y<<5)+16, 32, 32, 0, 0, 0, 127);
 					else if (shadeValue)
 						globalContainer->gfx->drawSprite((x<<5)+16, (y<<5)+16, globalContainer->terrainShader, shadeValue);
 				}
