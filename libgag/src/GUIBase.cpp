8099e671b13aa859be93fd485a884cae9418a30f
Bradley Arsenault <genixpro@gmail.com>
2008-02-20 21:26:24 -0500

    Made slight fix to last commit
    --HG--
    branch : beta2-rc

--- libgag/src/GUIBase.cpp
+++ libgag/src/GUIBase.cpp
@@ -482,14 +482,14 @@ namespace GAGGUI
 						SDLMod modState = SDL_GetModState();
 						//Manual integration of cmd+q and alt f4
 #						ifdef USE_OSX
-						if(event->key.keysym.sym == SDLK_q && modState & KMOD_META)
+						if(event.key.keysym.sym == SDLK_q && modState & KMOD_META)
 						{
 							run=false;
 							returnCode=-1;
 						}
 #						endif
 #						ifdef USE_WIN32
-						if(event->key.keysym..sym == SDLK_F4 && modState & KMOD_ALT)
+						if(event.key.keysym..sym == SDLK_F4 && modState & KMOD_ALT)
 						{
 							run=false;
 							returnCode=-1;
