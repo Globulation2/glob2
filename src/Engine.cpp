ddbfc19fb9e784f6618efa04690f494bbfd3c153
Bradley Arsenault <genixpro@gmail.com>
2008-03-31 19:30:55 -0400

    Fixed automaticEndingGame time calculation

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -417,8 +417,8 @@ int Engine::run(void)
 		if(globalContainer->automaticEndingGame)
 		{
 			int time = (automaticGameEndTick - startTime);
-			int seconds = ((automaticGameEndTick - startTime)/25) % 60;
-			int minutes = ((automaticGameEndTick - startTime)/25) / 60;
+			int seconds = ((automaticGameEndTick - startTime)/1000) % 60;
+			int minutes = ((automaticGameEndTick - startTime)/1000) / 60;
 			std::cout<< "automaticEndingGame ended: "<<time<<" ticks, "<<seconds<<" seconds, "<<minutes<<" minutes"<<std::endl;
 		}
 
