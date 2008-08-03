aac4083ec4dc57dd3aa6090029fdb02c3901e890
Martin Voelkle <martin dot voelkle at gmail dot com>
2008-08-03 17:06:35 +0200

    changed runtime interface

--- libusl/src/interpreter.cpp
+++ libusl/src/interpreter.cpp
@@ -1,6 +1,14 @@
 #include "interpreter.h"
-#include "types.h"
 
+#include "types.h"
+#include "usl.h"
+#include "code.h"
+
+#include <iostream>
+
+using namespace std;
+
+/*
 Thread::RuntimeValues::RuntimeValues():
 	trueValue(0),
 	falseValue(0)
@@ -21,7 +29,64 @@ Value* Thread::getRootLocal(const std::string& name)
 	size_t index = it - rootPrototype->locals.begin();
 	return root->locals[index];
 }
+*/
 
+Value* Thread::step()
+{
+	Thread::Frame& frame = frames.back();
+	ThunkPrototype* thunk = frame.thunk->thunkPrototype();
+	size_t nextInstr = frame.nextInstr;
+	Code* code = thunk->body[nextInstr];
+	frame.nextInstr++;
+	
+	cout << thunk;
+	for (size_t i = 0; i < frames.size(); ++i)
+		cout << "[" << frames[i].stack.size() << "]";
+	cout << " " << usl->debug.find(thunk, nextInstr) << ": ";
+	code->dump(cout);
+	cout << endl;
+	
+	code->execute(this);
+	
+	while (true)
+	{
+		Thread::Frame& frame = frames.back();
+		if (frame.nextInstr < frame.thunk->thunkPrototype()->body.size())
+			return 0;
+		Value* retVal = frame.stack.back();
+		frames.pop_back();
+		if (!frames.empty())
+		{
+			frames.back().stack.push_back(retVal);
+		}
+		else
+		{
+			return retVal;
+		}
+	}
+}
+
+Value* Thread::run(size_t& steps)
+{
+	while (steps)
+	{
+		Value* result = step();
+		--steps;
+		if (result)
+			return result;
+	}
+	return 0;
+}
+
+Value* Thread::run()
+{
+	while (true)
+	{
+		Value* result = step();
+		if (result)
+			return result;
+	}
+}
 
 void Thread::markForGC()
 {
