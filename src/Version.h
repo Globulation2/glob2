201966ffb56577a2ed4d4f73233ae5290801e11f
leo <leo.wandersleb@gmx.de>
2009-01-14 20:15:51 +0100

    loading, saving, initialisation and versionMinor for canNotConvertUnitTimer

--- src/Version.h
+++ src/Version.h
@@ -23,7 +23,7 @@
 // This is the version of map and savegame format, and all of the recorded datas on the server
 #define VERSION_MAJOR 0
 #define MINIMUM_VERSION_MINOR 58
-#define VERSION_MINOR 80
+#define VERSION_MINOR 81
 // version 10 adds script saved in game
 // version 11 the gamesfiles do saves which building has been seen under fog of war.
 // version 12 saves map name into SessionGame instead of BaseMap.
@@ -95,6 +95,7 @@
 // version 78 added defaultFlagRadius to the Order class
 // version 79 added priority to the Building class
 // version 80 added unitsHarvesting list to the Building class
+// version 81 added canNotConvertUnitTimer to the Building class
 
 //This must be updated when there are changes to YOG, MapHeader, GameHeader, BasePlayer, BaseTeam,
 //NetMessage, and the likes, in parrallel to change of the VERSION_MINOR above
