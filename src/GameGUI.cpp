efe97fcf41c1f328a96add3f953bb3c9b72983e1
Stephane Magnenat <stephane at magnenat dot net@localhost>
2009-08-16 13:56:30 +0200

    Cleaned-up structure for displaying off-screen units. This fixes a display bug when using low quality graphics.

--- src/GameGUI.cpp
+++ src/GameGUI.cpp
@@ -4009,7 +4009,7 @@ void GameGUI::drawAll(int team)
 	if (globalContainer->settings.optionFlags & GlobalContainer::OPTION_LOW_SPEED_GFX)
 	{
 		globalContainer->gfx->setClipRect(0, 16, globalContainer->gfx->getW()-RIGHT_MENU_WIDTH, globalContainer->gfx->getH()-16);
-		game.drawMap(0, 0, globalContainer->gfx->getW()-RIGHT_MENU_WIDTH, globalContainer->gfx->getH(),viewportX, viewportY, localTeamNo, drawOptions);
+		game.drawMap(0, 0, globalContainer->gfx->getW()-RIGHT_MENU_WIDTH, globalContainer->gfx->getH(), 0, 16, viewportX, viewportY, localTeamNo, drawOptions);
 	}
 	else
 	{
@@ -4017,7 +4017,7 @@ void GameGUI::drawAll(int team)
 		
 		globalContainer->gfx->setClipRect();
 		
-		game.drawMap(0, 0, globalContainer->gfx->getW(), globalContainer->gfx->getH(),viewportX, viewportY, localTeamNo, drawOptions, &visibleBuildings);
+		game.drawMap(0, 0, globalContainer->gfx->getW(), globalContainer->gfx->getH(), RIGHT_MENU_WIDTH, 16, viewportX, viewportY, localTeamNo, drawOptions, &visibleBuildings);
 		
 		// generate and draw particles
 		generateNewParticles(&visibleBuildings);
