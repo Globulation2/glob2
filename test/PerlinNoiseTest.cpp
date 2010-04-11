85f47e7760efa5d9048fb169a8c79cd752fb7d88
Leo Wandersleb <Leo@LeoWandersleb.de>
2010-04-11 03:47:02 +0200

    PerlinNoiseTest comments, new tests

--- test/PerlinNoiseTest.cpp
+++ test/PerlinNoiseTest.cpp
@@ -15,8 +15,6 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-
-
 #include "PerlinNoiseTest.h"
 #include "../src/PerlinNoise.h"
 CPPUNIT_TEST_SUITE_REGISTRATION( PerlinNoiseTest );
@@ -25,6 +23,7 @@ const static int SEED = 12;
 PerlinNoise * perlinNoise;
 float * position;
 
+//initialize perlinNoise with always the same seed and setUp a positon-vector.
 void PerlinNoiseTest::setUp()
 {
 	perlinNoise = new PerlinNoise(SEED);
@@ -48,6 +47,7 @@ void PerlinNoiseTest::testNotZeroOne()
 	CPPUNIT_ASSERT(a != 0.0f);
 	CPPUNIT_ASSERT(a != 1.0f);
 }
+//via resetting the seed, noise should generate different values
 void PerlinNoiseTest::testReseed()
 {
 	float a = perlinNoise->Noise1d(position);
@@ -55,25 +55,37 @@ void PerlinNoiseTest::testReseed()
 	float b = perlinNoise->Noise1d(position);
 	CPPUNIT_ASSERT(a != b);
 }
-void PerlinNoiseTest::testReseedInt()
+//via resetting the seed to different values, noise should generate different values
+void PerlinNoiseTest::testReseedIntDifferent()
 {
-	float a = perlinNoise->Noise1d(position);
-	perlinNoise->reseed(SEED);
-	float b = perlinNoise->Noise1d(position);
-	CPPUNIT_ASSERT_EQUAL(a, b);
+	float valueWithOriginalSeed = perlinNoise->Noise1d(position);
+	perlinNoise->reseed(SEED + 3);
+	float valueWithSomeOtherSeed = perlinNoise->Noise1d(position);
+	CPPUNIT_ASSERT(valueWithOriginalSeed != valueWithSomeOtherSeed);
 }
+//via resetting the seed to what it was, noise should regenerate same values
+void PerlinNoiseTest::testReseedIntSame()
+{
+	float valueWithOriginalSeed = perlinNoise->Noise1d(position);
+	perlinNoise->reseed(SEED);
+	float valueWithReseededOriginalSeed = perlinNoise->Noise1d(position);
+	CPPUNIT_ASSERT_EQUAL(valueWithOriginalSeed, valueWithReseededOriginalSeed);
+}
+//the means to access 1d-noise should result in the same value
 void PerlinNoiseTest::testnoise1d()
 {
 	float a = perlinNoise->Noise1d(position);
 	float b = perlinNoise->Noise(position[0]);
 	CPPUNIT_ASSERT_EQUAL(a, b);
 }
+//the means to access 2d-noise should result in the same value
 void PerlinNoiseTest::testnoise2d()
 {
 	float a = perlinNoise->Noise2d(position);
 	float b = perlinNoise->Noise(position[0], position[1]);
 	CPPUNIT_ASSERT_EQUAL(a, b);
 }
+//the means to access 3d-noise should result in the same value
 void PerlinNoiseTest::testnoise3d()
 {
 	float a = perlinNoise->Noise3d(position);
