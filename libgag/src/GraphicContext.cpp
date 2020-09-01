300ca726c19ad3861afbd6ff7c88c235369de9f3
Nathan Mills <38995150+Quipyowert2@users.noreply.github.com>
2020-08-31 19:29:58 -0700

    Fix pink cursor (should be red like with SDL1.2)

--- libgag/src/GraphicContext.cpp
+++ libgag/src/GraphicContext.cpp
@@ -2109,21 +2109,8 @@ namespace GAGCore
 		optionFlags &= ~USEGPU;
 		#endif
 
-		// create surface
-		Uint32 rmask, gmask, bmask, amask;
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-	rmask = 0xff000000;
-	gmask = 0x00ff0000;
-	bmask = 0x0000ff00;
-	amask = 0x000000ff;
-#else
-	rmask = 0x000000ff;
-	gmask = 0x0000ff00;
-	bmask = 0x00ff0000;
-	amask = 0xff000000;
-#endif
-		sdlsurface = SDL_CreateRGBSurface(0, w, h, 32, rmask, gmask, bmask, amask);
 		SDL_SetWindowSize(window, w, h);
+		sdlsurface = SDL_GetWindowSurface(window);
 
 		// check surface
 		if (!sdlsurface)
