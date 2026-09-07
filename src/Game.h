7976bfc21efdc73e910ee161ec5ee472313e2d9a
Bradley Arsenault <brad@bradleyarsenault.me>
2026-09-07 19:39:47 -0400

    Clamp status-bar fills instead of aborting on overflow

--- src/Game.h
+++ src/Game.h
@@ -119,6 +119,7 @@ static constexpr int GRADIENT_DIRTY_BORDER_TILES = 16;
 
 class Game
 {
+	friend class PointBarRenderTest;
 	static const bool verbose = false;
 public:
 	/// Per-client viewer state (selection + mouse). Defined below; forward-
